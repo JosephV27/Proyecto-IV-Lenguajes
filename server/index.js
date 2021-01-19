7d92c927d95ea1c35bd3007a13a588c4d994bb03
JosephV27 <jvalencianom08@yahoo.com>
2021-01-18 18:20:06 -0600

    agregando servidor con express js

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+// Settings 
+app.set('port', process.env.PORT || 9000);
+
+// Middlewares - Funciones que se ejecutan antes de que lleguen a las rutas
+// app.use(morgan('dev'));
+app.use(express.json());
+
+// Routes  
+// app.use('/api/juego', require('./server/routes/routes'));
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+// Static Files
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// Starting the server
+app.listen(app.get('port'), () => {
+    console.log(`server on port ${app.get('port')}`);
+});
