08ff53182db46ee8565124ace44fe2a8fb5564f9
JosephV27 <jvalencianom08@yahoo.com>
2021-01-20 18:03:45 -0600

    quitando warnings

--- client/src/components/MainPage.js
+++ client/src/components/MainPage.js
@@ -6,7 +6,7 @@ function MainPage() {
     return (
         <div className="main_page">
             <h2 className="entry-title">Bienvenido a Luiki Kart!!!</h2>
-            <img src={luikiKart} height="840"/>
+            <img src={luikiKart} height="840" alt="main logo"/>
         </div>
     );
 }
