adc6e30e1462f659164ee06cfc7609839e59c1fc
JosephV27 <jvalencianom08@yahoo.com>
2021-01-20 21:50:37 -0600

    template de tabla

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Navigation from './components/Navigation';
 import MainPage from './components/MainPage';
 import CreateGame from './components/CreateGame';
+import Game from './components/Game';
 
 function App() {
   return (
-	<Router>
-		<Navigation/>
-		<Route path="/" exact component={MainPage} />
-		<Route path="/crear-partida" component={CreateGame} />
-	</Router>
+	// <Router>
+	// 	<Navigation/>
+	// 	<Route path="/" exact component={MainPage} />
+	// 	<Route path="/crear-partida" component={CreateGame} />
+	// 	<Route path="/juego" component={Game} />
+	// </Router>
+	<Game/>
   );
 }
 
